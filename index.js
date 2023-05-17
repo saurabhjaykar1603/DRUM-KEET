a1e3af245763b7140e543070579460e1858cf5fe
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-17 10:05:43 +0530

    initial changes

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
-for (let i = 0; i<numberOfDrumButtons; i++){
+for (let i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click",
-    function handleClick() {
+        function handleClick() {
 
-        let buttonInnerHTML = this.innerHTML;
+            let buttonInnerHTML = this.innerHTML;
 
-        switch (buttonInnerHTML) {
-            case "w":
-                let tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play()
-                break;
+            switch (buttonInnerHTML) {
+                case "w":
+                    let tom1 = new Audio('sounds/tom-1.mp3');
+                    tom1.play()
+                    break;
 
                 case "a":
                     let tom2 = new Audio('sounds/tom-2.mp3');
                     tom2.play()
                     break;
 
-                    case "s":
-                        let tom3 = new Audio('sounds/tom-3.mp3');
-                        tom3.play()
-                        break;
+                case "s":
+                    let tom3 = new Audio('sounds/tom-3.mp3');
+                    tom3.play()
+                    break;
 
-                        case "d":
-                            let tom4 = new Audio('sounds/tom-4.mp3');
-                            tom4.play()
-                            break;
+                case "d":
+                    let tom4 = new Audio('sounds/tom-4.mp3');
+                    tom4.play()
+                    break;
 
-                            case "j":
-                                let snare = new Audio('sounds/snare.mp3');
-                                snare.play()
-                                break;
+                case "j":
+                    let snare = new Audio('sounds/snare.mp3');
+                    snare.play()
+                    break;
 
-                                case "k":
-                                    let crash = new Audio('sounds/crash.mp3');
-                                    crash.play()
-                                    break;
+                case "k":
+                    let crash = new Audio('sounds/crash.mp3');
+                    crash.play()
+                    break;
 
-                                    case "l":
-                                        let kick = new Audio('sounds/kick-bass.mp3');
-                                        kick.play()
-                                        break;
+                case "l":
+                    let kick = new Audio('sounds/kick-bass.mp3');
+                    kick.play()
+                    break;
 
 
-            default:
-        }
-        //what to do whem click 
-    });
+                default: console.log();
+
+            }
+            //what to do whem click 
+        });
 
 }
 
