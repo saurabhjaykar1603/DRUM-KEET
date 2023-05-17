95e80b14b6b5709ca58ecf7fbe30b669a1d2778f
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-17 08:50:40 +0530

    drum sett html css added and Eventlistener func with for loop creating for click handling on images

---
+++ index.js
@@ -0,0 +1,16 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (let i = 0; i<numberOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",
+    function handleClick() {
+        alert("I got click");
+        //what to do whem click 
+    });
+
+}
+
+
+
+
+
+
