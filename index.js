adb25bb9b1606d4baa4a50c84a762681c98f0573
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-17 09:16:17 +0530

    audio playing with js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ for (let i = 0; i<numberOfDrumButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click",
     function handleClick() {
-        alert("I got click");
+        
+        let audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
         //what to do whem click 
     });
 
